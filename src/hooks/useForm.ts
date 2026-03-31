ef1373f943ce26dc449235dcc38135fde6b639bd
Thiago Botelho <thiagoboth@hotmail.com>
2026-03-30 23:45:15 -0300

    Preparando para deploy

--- src/hooks/useForm.ts
+++ src/hooks/useForm.ts
@@ -26,7 +26,7 @@ interface UseFormReturn<T> {
  * useForm - Hook para gerenciamento de formulários
  * Inclui validação, estados de loading/success/error
  */
-export function useForm<T extends { [key: string]: string }>({
+export function useForm<T extends { [K in keyof T]: string }>({
   initialValues,
   onSubmit,
   validate,
